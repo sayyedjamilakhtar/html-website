b38980bfaaf4d4e3cde882a46e806f529d1520ab
sayyedjamilakhtar <143558873+sayyedjamilakhtar@users.noreply.github.com>
2023-11-16 02:28:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// "use strict";
+
+const arr = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    console.log(arr[i], [j]);
+  }
+}
+
+console.log([1, 2][0]);
